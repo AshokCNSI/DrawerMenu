3ce603332b6b989c3fc0573018207bd25116b8b8
Ashokkumar <igasmr@gmail.com>
2016-04-05 08:39:16 +0530

    Js Files

--- www/js/app.js
+++ www/js/app.js
@@ -5,16 +5,37 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic','ionic.contrib.drawer','starter.controllers', 'starter.services'])
+angular.module('starter', ['ionic','ionic.contrib.drawer','starter.controllers', 'starter.services','ngCordova'])
 
-.run(function($ionicPlatform) {
+.run(function($ionicPlatform,$ionicPopup) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
+	if(window.Connection) {
+        if(navigator.connection.type == Connection.NONE) {
+            $ionicPopup.confirm({
+                title: "Internet Disconnected",
+                ontent: "The internet is disconnected on your device."
+            })
+            .then(function(result) {
+                if(!result) {
+                    ionic.Platform.exitApp();
+                    }else{
+					ionic.Platform.exitApp();
+					}
+                });
+			}
+    }
+	
+	$ionicPlatform.registerBackButtonAction(function(event) {
+    if (true) { // your check here
+      ionic.Platform.exitApp();
+    }
+  }, 100);
+  
     if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
       cordova.plugins.Keyboard.disableScroll(true);
-
     }
     if (window.StatusBar) {
       // org.apache.cordova.statusbar required
@@ -31,51 +52,61 @@ angular.module('starter', ['ionic','ionic.contrib.drawer','starter.controllers',
   // Each state's controller can be found in controllers.js
   $stateProvider
 
-  /*// setup an abstract state for the tabs directive
-    .state('tab', {
-    url: '/tab',
-    abstract: true,
-    templateUrl: 'templates/tabs.html'
-  })*/
-
-  // Each tab has its own nav history stack:
-
-  .state('dash', {
-    url: '/dash',
-    templateUrl: 'templates/tab-dash.html',
-    controller: 'DashCtrl'
+  .state('main', {
+    url: '/main',
+    templateUrl: 'templates/main.html',
+    controller: 'MainCtrl'
+  })
+  
+  .state('home', {
+    url: '/home',
+    templateUrl: 'templates/home.html',
+    controller: 'HomeCtrl'
+  })
+  
+  .state('login', {
+    url: '/login',
+    templateUrl: 'templates/login.html',
+    controller: 'LoginCtrl'
+  })
+  
+  .state('registration', {
+    url: '/registration',
+    templateUrl: 'templates/registration.html',
+    controller: 'RegistrationCtrl'
   })
 
-  .state('chats', {
-      url: '/chats',
-      views: {
-        'chats': {
-          templateUrl: 'templates/tab-chats.html',
-          controller: 'ChatsCtrl'
-        }
-      }
-    })
-    .state('chat-detail', {
-      url: '/chats/:chatId',
-      views: {
-        'chats': {
-          templateUrl: 'templates/chat-detail.html',
-          controller: 'ChatDetailCtrl'
-        }
-      }
-    })
-
-  .state('account', {
-    url: '/account',
-    views: {
-      'account': {
-        templateUrl: 'templates/tab-account.html',
-        controller: 'AccountCtrl'
-      }
-    }
+  .state('bookingHistory', {
+    url: '/bookingHistory',
+    templateUrl: 'templates/booking-history.html',
+    controller: 'BookingHistoryCtrl'
+  })
+  
+  .state('offerCoupons', {
+    url: '/offerCoupons',
+    templateUrl: 'templates/offer-coupons.html',
+    controller: 'OfferCouponsCtrl'
+  })
+  
+  .state('referEarn', {
+    url: '/referEarn',
+    templateUrl: 'templates/refer-earn.html',
+    controller: 'ReferEarnCtrl'
+  })
+  
+  .state('helpFaq', {
+    url: '/helpFaq',
+    templateUrl: 'templates/help-faq.html',
+    controller: 'HelpFaqCtrl'
+  })
+  
+  .state('contactUs', {
+    url: '/contactUs',
+    templateUrl: 'templates/contact-us.html',
+    controller: 'ContactUsCtrl'
   });
 
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/dash');
+  $urlRouterProvider.otherwise('/main');
 
 });
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,8 +1,43 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
+.controller('drawerController', function($scope,$location) {
+	$('body').find('.sideMenu').css('display','none');
+	$scope.passView = function(viewName){
+		$scope.closeDrawer();
+		$location.path('/'+viewName)
+	}
+})
 
-.controller('ChatsCtrl', function($scope, Chats) {
+.controller('MainCtrl', function($scope,$location) {
+	$scope.passView = function(viewName){
+		$location.path('/'+viewName)
+	}
+})
+
+.controller('HomeCtrl', function($scope) {
+	$('body').find('.sideMenu').css('display','block');
+	if(window.StatusBar) {
+	  StatusBar.overlaysWebView( false );
+	  StatusBar.backgroundColorByHexString('#5f9ea0');
+	  StatusBar.styleLightContent();
+    }
+	
+	$('.bxslider').bxSlider({
+		auto : true,
+		captions: true
+	});
+	
+})
+
+.controller('LoginCtrl', function($scope) {
+	
+})
+
+.controller('RegistrationCtrl', function($scope) {
+	
+})
+
+.controller('BookingHistoryCtrl', function($scope, Chats) {
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
   // To listen for when this page is active (for example, to refresh data),
@@ -17,12 +52,20 @@ angular.module('starter.controllers', [])
   };
 })
 
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+.controller('OfferCouponsCtrl', function($scope, $stateParams, Chats) {
   $scope.chat = Chats.get($stateParams.chatId);
 })
 
-.controller('AccountCtrl', function($scope) {
+.controller('ReferEarnCtrl', function($scope) {
   $scope.settings = {
     enableFriends: true
   };
+})
+
+.controller('HelpFaqCtrl', function($scope, $stateParams, Chats) {
+  $scope.chat = Chats.get($stateParams.chatId);
+})
+
+.controller('ContactUsCtrl', function($scope, $stateParams, Chats) {
+  $scope.chat = Chats.get($stateParams.chatId);
 });
